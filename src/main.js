24dd6c064c870df67262eaa50d676373aa2debb4
racercode <convenient0610@gmail.com>
2022-07-11 20:39:40 +0800

    add csrf token

--- src/main.js
+++ src/main.js
@@ -11,6 +11,14 @@ const app = createApp(App)
 
 axios.defaults.baseURL = 'https://vocabulary-learn-api.herokuapp.com/'
 
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-fromurlencodeed';
+axios.defaults.withCredentials = false;
+axios.interceptors.request.use((config) => {
+  config.headers['X-Requested-With'] = 'XMLHttpRequest';
+  config.headers['X-CSRFToken'] = cookie.parse(document.cookie).csrftoken;
+  return config
+});
+
 app.use(createPinia())
 app.use(router)
 
